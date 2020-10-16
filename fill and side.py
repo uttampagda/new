d6fded04585d848c99bc70287f9ab9e1d2a34a0e
uttampagda89 <72185325+uttampagda89@users.noreply.github.com>
2020-10-16 11:04:57 +0530

    change in geometry and color

--- fill and side.py
+++ fill and side.py
@@ -1,12 +1,12 @@
 from tkinter import *
 root = Tk()
-root.geometry("700x400")
-one= Label(root,text="one",fg="white",bg="black")
+root.geometry("750x450")
+one= Label(root,text="One",fg="white",bg="blue")
 one.pack()
-two= Label(root,text="two",fg="red",bg="black")
+two= Label(root,text="two",fg="black",bg="black")
 two.pack(fill=X)
 three= Label(root,text="three",fg="yellow",bg="red")
-three.pack(side=LEFT,fill=Y)
+three.pack(side=LEFT,fill=y)
 root.mainloop()
 
 
